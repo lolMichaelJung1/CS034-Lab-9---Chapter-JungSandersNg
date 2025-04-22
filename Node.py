41e2c51fb56fa2a5fd737abb473dfb702a822497
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-21 21:35:43 -0700

    Update Node.py

--- Node.py
+++ Node.py
@@ -7,3 +7,20 @@ class Node:
 
   def __str__(self):
       return f"Node (key={self.key})"
+
+# -----------------------
+# Command Line Interface
+# -----------------------
+if __name__ == "__main__":
+    root = Node(10)
+    root.left = Node(5)
+    root.right = Node(15)
+    root.left.left = Node(2)
+    root.left.right = Node(7)
+
+    print("DEBUG: Successfully Insert Nodes!")
+    print(root)
+    print(root.left)
+    print(root.right)
+    print(root.left.left)
+    print(root.left.right)
