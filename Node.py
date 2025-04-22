705a92bcab2f3b7e9a1c4e799a234ebed44901b6
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-21 21:23:42 -0700

    Create Node.py

---
+++ Node.py
@@ -0,0 +1,9 @@
+class Node:
+  ''' Tree node with left and right child'''
+  def __init__(self, key):
+      self.key = key
+      self.right = None
+      self.right = None
+
+  def __str__(self):
+      return f"Node (key={self.key})"
