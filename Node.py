94bd19c9a3f1dcab400e7a29604ee5e3ec8572dc
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-23 16:57:27 -0700

    Update Node.py

--- Node.py
+++ Node.py
@@ -7,30 +7,3 @@ class Node:
 
   def __str__(self):
       return f"Node (key={self.key})"
-
-# -----------------------
-# Command Line Interface
-# -----------------------
-
-'''
-     A
-    / \
-   B   C
-  / \   \
- D   E   F
-'''
-if __name__ == "__main__":
-    root = Node('A')
-    root.left = Node('B')
-    root.right = Node('C')
-    root.left.left = Node('D')
-    root.left.right = Node('E')
-    root.right.right = Node('F')
-
-    print("DEBUG: Successfully Insert Nodes!")
-    print(root)
-    print(root.left)
-    print(root.right)
-    print(root.left.left)
-    print(root.left.right)
-    print(root.right.right)
