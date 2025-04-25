2a6ce9163b98ea1201b61e62d785ea96f0890558
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-24 19:03:47 -0700

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,3 +1,16 @@
+
+#--------------------------------------------------------------------------------------------
+# Group Members: Michael Jung (ID:10680322), Timothy Sanders (??), Megan Ng (ID: 00756276)
+
+# Date: 4/21/25
+
+# Course: Spr25_CS_034 CRN 39575
+#--------------------------------------------------------------------------------------------
+
+
+
+
+
 from __future__ import annotations
 from typing import Optional
 
