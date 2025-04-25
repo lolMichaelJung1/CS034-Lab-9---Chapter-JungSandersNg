510cdd3d24db3ef68e9e5ba46229ba921d67ff77
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-24 19:12:04 -0700

    Update BST.py

--- BST.py
+++ BST.py
@@ -1,11 +1,10 @@
-
-#--------------------------------------------------------------------------------------------
 # Group Members: Michael Jung (ID:10680322), Timothy Sanders (??), Megan Ng (ID: 00756276)
 
 # Date: 4/21/25
 
 # Course: Spr25_CS_034 CRN 39575
 #--------------------------------------------------------------------------------------------
+from TreeNode import TreeNode
 from BinaryTree import BinaryTree
 
 
