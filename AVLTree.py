3c93d046ee57565c6fbaa0f44d069dd9ef5a7595
Michael Jung <96927527+lolMichaelJung1@users.noreply.github.com>
2025-04-25 10:12:41 -0700

    Update AVLTree.py

--- AVLTree.py
+++ AVLTree.py
@@ -1,1 +1,24 @@
+# Group Members: Michael Jung (ID:10680322), Timothy Sanders (01002147), Megan Ng (ID: 00756276)
+
+# Date: 4/21/25
+
+# Course: Spr25_CS_034 CRN 39575
+#--------------------------------------------------------------------------------------------
+# Create a test tree manually
+#          13
+#        /    \
+#       6     21
+#     /   \   /
+#    5    12  18
+#   /
+#  4
+# Print AVL with indentation
+# Print the nodes pre-order, in-order, post-order to visualize the balancing of tree after the deletion and insertion of nodes
+# ######################################################
+
+from Node import Node
+
+# --- AVL Tree Class ---
+class AVLTree():
+
 
