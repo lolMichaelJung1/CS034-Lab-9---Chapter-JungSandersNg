8d4b297683c03271bff7f540031282e7451183df
CodingforMegan <79181396+CodingforMegan@users.noreply.github.com>
2025-04-26 22:18:53 -0700

    successfully integrate unit test with print demo __main__ block
    I have tested it several times on google colab, it works fine except the AVLTree is simplified version (only including overide methods of insert(), print_tree()

--- module9_lab.py
+++ module9_lab.py
@@ -20,7 +20,7 @@ class Node:
         self.value = value      # Store data in the node
         self.left = None        # Pointer to the left child node
         self.right = None       # Pointer to the right child node
-        self.height = 0         # Height of the node (used in AVL trees for balancing)
+        self.height = 1         # Height of the node (used in AVL trees for balancing)
 
     def __str__(self):
         # Return a string representation of the node's value
